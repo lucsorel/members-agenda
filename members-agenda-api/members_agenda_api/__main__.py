227edde411efee585da63505829e5734402d2107
Luc Sorel-Giffo <luc.sorel@gmail.com>
2024-06-24 10:07:07 +0200

    refactor(api): create a db connection per api call to allow concurrent calls

--- members-agenda-api/members_agenda_api/__main__.py
+++ members-agenda-api/members_agenda_api/__main__.py
@@ -4,25 +4,11 @@ from logging import getLogger
 from fastapi import FastAPI
 
 from members_agenda_api.api import API_ROUTER
-from members_agenda_api.services import load_services, get_data_service
 
 
 LOGGER = getLogger('uvicorn')
 
-@asynccontextmanager
-async def app_lifespan(app: FastAPI):
-    # loads the services used by the application
-    LOGGER.info('- Starting web API, loading services...')
-    load_services()
-    LOGGER.info('- Services loaded')
-    yield
-
-    # Clean up the ML models and release the resources
-    LOGGER.info('- API server shutting down, closing db connection...')
-    get_data_service().mysql_connection.close()
-    LOGGER.info('- Database connection closed')
-
-app = FastAPI(lifespan=app_lifespan)
+app = FastAPI()
 
 @app.get('/')
 def read_root():
--- members-agenda-api/members_agenda_api/services/__init__.py
+++ members-agenda-api/members_agenda_api/services/__init__.py
@@ -11,30 +11,17 @@ MEMBERS_AGENDA_DATABASE = getenv('MEMBERS_AGENDA_DATABASE')
 MEMBERS_AGENDA_USER = getenv('MEMBERS_AGENDA_USER')
 MEMBERS_AGENDA_PASSWORD = getenv('MEMBERS_AGENDA_PASSWORD')
 
-
-_DATA_SERVICE: DataService = None
-_PERSON_SERVICE: PersonService = None
-
-def get_person_service() -> PersonService:
-    return _PERSON_SERVICE
+def get_connection() -> Connection:
+    return Connection(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MEMBERS_AGENDA_USER,
+        password=MEMBERS_AGENDA_PASSWORD,
+        database=MEMBERS_AGENDA_DATABASE,
+    )
 
 def get_data_service() -> DataService:
-    return _DATA_SERVICE
+    return DataService(get_connection())
 
-def load_services():
-    # instantiates the application services once for all
-    global _DATA_SERVICE, _PERSON_SERVICE
-
-    if not _DATA_SERVICE:
-        _DATA_SERVICE = DataService(
-            Connection(
-                host=MYSQL_HOST,
-                port=MYSQL_PORT,
-                user=MEMBERS_AGENDA_USER,
-                password=MEMBERS_AGENDA_PASSWORD,
-                database=MEMBERS_AGENDA_DATABASE,
-            )
-        )
-
-    if not _PERSON_SERVICE:
-        _PERSON_SERVICE = PersonService(_DATA_SERVICE)
+def get_person_service() -> PersonService:
+    return PersonService(get_data_service())
