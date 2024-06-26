f9ad47b179679c1a76de1d962b0b5c3b2f82060d
Luc Sorel-Giffo <luc.sorel@gmail.com>
2024-06-26 02:56:39 +0200

    test(api): renumber the test modules

--- members-agenda-api/tests/conftest.py
+++ members-agenda-api/tests/conftest.py
@@ -10,19 +10,16 @@ from tests.containers.sql_helper import SqlTestHelper
 @fixture(scope="session")
 def sql_test_helper() -> SqlTestHelper:
     db_name = 'members_agenda'
-    test_user = 'members_agenda_test'
-    test_user_password = 'I_l0v3_t35t5'
+
     # Docker container creation
     with MySqlContainer(
         image="mysql:8.0",
-        username=test_user,
-        password=test_user_password,
         dbname=db_name,
     ) as sql_container:
         # database connection (for direct use or monkeypatching in tests)
         connection = Connection(
-            user=test_user,
-            password=test_user_password,
+            user=sql_container.username,
+            password=sql_container.password,
             host=sql_container.get_container_host_ip(),
             port=int(sql_container.get_exposed_port(sql_container.port)),
             database=db_name,
